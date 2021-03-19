2fda1fb9eacede5e80d95d454eb70ab9cf6f3866
Cleber <cleberof@msn.com>
2021-03-19 16:32:18 -0300

    composer

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
+require __DIR__ . "/vendor/autoload.php";
+
 echo '<h1>Ola Mundo</h1>';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vendor/teste',
+  ),
+  'versions' => 
+  array (
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.0.7',
+      'version' => '6.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c41a36d4508d470e376498c1c0c527aa36a2d59',
+    ),
+    'vendor/teste' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
